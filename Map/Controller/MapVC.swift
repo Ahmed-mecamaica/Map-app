523948f8299e4120fb46c478de12019a28c4cc73
Ahmed Abou-Zeid <ahmedmaika@gmail.com>
2020-01-19 15:11:21 +0200

    remove spinner,progressLbl and add progresslbl,add collection view programatically

--- Map/Controller/MapVC.swift
+++ Map/Controller/MapVC.swift
@@ -17,6 +17,8 @@ class MapVC: UIViewController,UIGestureRecognizerDelegate {
     var regionRadius: Double = 1000
     var spinner: UIActivityIndicatorView?
     var progressLbl: UILabel?
+    var flowLayout = UICollectionViewFlowLayout()
+    var collectionView: UICollectionView?
     var screenSize = UIScreen.main.bounds
     @IBOutlet weak var pullUpView: UIView!
     @IBOutlet weak var heightViewConstraint: NSLayoutConstraint!
@@ -28,6 +30,12 @@ class MapVC: UIViewController,UIGestureRecognizerDelegate {
         mapView.delegate = self
         configurLocationService()
         addDoubleTap()
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: flowLayout)
+        collectionView?.register(PhotoCell.self, forCellWithReuseIdentifier: "photoCell")
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        collectionView?.backgroundColor = #colorLiteral(red: 1, green: 0.6241641045, blue: 0, alpha: 1)
+        pullUpView.addSubview(collectionView!)
     }
     
     func addDoubleTap(){
@@ -61,14 +69,36 @@ class MapVC: UIViewController,UIGestureRecognizerDelegate {
             self.view.layoutIfNeeded()
         }
     }
+    
     func addSpinner(){
     spinner = UIActivityIndicatorView()
         spinner?.center = CGPoint(x: (screenSize.width / 2)-((spinner?.frame.width)! / 2), y: 150)
         spinner?.style = .whiteLarge
         spinner?.color = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         spinner?.startAnimating()
-        pullUpView.addSubview(spinner!)
+        collectionView?.addSubview(spinner!)
         
+    }
+    
+    func removeSpinner(){
+        if spinner != nil{
+            spinner?.removeFromSuperview()
+        }
+    }
+    
+    func addProgressLbl(){
+        progressLbl = UILabel()
+        progressLbl?.frame = CGRect(x: (screenSize.width / 2) - 100, y: 165, width: 200, height: 40)
+        progressLbl?.textAlignment = .center
+        progressLbl?.text = "PHOTOS LOAD..."
+        progressLbl?.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        collectionView?.addSubview(progressLbl!)
+    }
+    
+    func removeProgressLbl(){
+        if progressLbl != nil{
+            progressLbl?.removeFromSuperview()
+        }
     }
 }
 extension MapVC: MKMapViewDelegate{
@@ -91,9 +121,12 @@ extension MapVC: MKMapViewDelegate{
     
     @objc func dropPin(sender: UIGestureRecognizer){
         removePin()
+        removeSpinner()
+        removeProgressLbl()
         animatViewUp()
         swapping()
         addSpinner()
+        addProgressLbl()
         
         let touchPoint = sender.location(in: mapView)
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
@@ -124,3 +157,19 @@ extension MapVC: CLLocationManagerDelegate{
     }
 }
 
+extension MapVC: UICollectionViewDelegate, UICollectionViewDataSource{
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? PhotoCell
+        return cell!
+    }
+    
+    
+}
---
+++ Map/View/PhotoCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.swift
+//  Map
+//
+//  Created by maika on 1/19/20.
+//  Copyright © 2020 maika. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init (coder:) has not been implemented ")
+    }
+}
